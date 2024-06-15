635893ad0e3475823f447d79d9d330f0bb82b9a9
Andy Kipp <anki-code@users.noreply.github.com>
2024-06-15 18:25:01 +0200

    Update main.py

--- project-template/{{ project_repo_name }}/{%if enable_autoloading%}{{ py_module_name }}{%endif%}/main.py
+++ project-template/{{ project_repo_name }}/{%if enable_autoloading%}{{ py_module_name }}{%endif%}/main.py
@@ -2,16 +2,17 @@ from xonsh.built_ins import XonshSession
 
 
 def _load_xontrib_(xsh: XonshSession, **_):
-    # getting environment variable
-    var = xsh.env.get("VAR", "default")
 
-    print("Autoloading xontrib: {{ project_repo_name }}")
+    # If your xontrib was made only for interactive mode it's good practice to check this before executing.
+    if XSH.env.get("XONSH_INTERACTIVE"):
+        var = xsh.env.get("VAR", "default")
+        print("Autoloading xontrib: {{ project_repo_name }}")
 
-    {% if has_events %}
-    from .event_hooks import listen_cd
-    xsh.builtins.events.on_chdir(listen_cd)
-    {% endif %}
-    {% if has_aliases %}
-    from .aliases import git_log
-    xsh.aliases["gl"] = git_log
-    {% endif %}
+        {% if has_events %}
+        from .event_hooks import listen_cd
+        xsh.builtins.events.on_chdir(listen_cd)
+        {% endif %}
+        {% if has_aliases %}
+        from .aliases import git_log
+        xsh.aliases["gl"] = git_log
+        {% endif %}
